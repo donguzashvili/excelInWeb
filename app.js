d0916311a9381190520ff4d7857ba34e91219fd8
donguzashvili <shota.nope@gmail.com>
2021-07-30 17:59:53 +0400

    feat: updateN3

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let regex = "";
 let columnNum = 0;
 let id = 15;
 let table = null;
+let dataTable;
+let nameArray = [];
 
 function fetchData(url) {
   try {
@@ -16,46 +18,6 @@ function fetchData(url) {
   }
 }
 
-function styleHtml() {
-  //create footer and header
-  const header = document.createElement("header");
-  const footer = document.createElement("footer");
-
-  const dataTableWrapper = document.getElementById("myTable_wrapper");
-  const dataTableLength = document.getElementById("myTable_length");
-  const dataTableFilter = document.getElementById("myTable_filter");
-  const dataTablePaginate = document.getElementById("myTable_paginate");
-  const dataTableInfo = document.getElementById("myTable_info");
-
-  //append elements to footer and header
-  header.append(dataTableLength);
-  header.append(dataTableFilter);
-  footer.classList.add("dataTables_wrapper");
-  footer.append(dataTableInfo);
-  footer.append(dataTablePaginate);
-  dataTableWrapper.prepend(header);
-  document.body.append(footer);
-  scroll();
-  fixedHeader();
-}
-function scroll() {}
-
-function fixedHeader() {
-  //take tables offset and tableHead
-  let table = document.getElementById("myTable");
-  let tHead = document.getElementsByTagName("thead");
-
-  //on scroll move header with content like a position fixed
-  $(window).bind("scroll", function () {
-    let offset = $(this).scrollTop();
-    if (offset >= table.offsetTop) {
-      let position = offset - 45;
-      tHead[0].style.cssText = "transform: translateY(" + position + "px)";
-    } else if (offset < table.offsetTop) {
-      tHead[0].style.cssText = "transform: translateY(-45px)";
-    }
-  });
-}
 let tHeadID = 0;
 
 function objectCreate(data) {
@@ -77,15 +39,59 @@ function objectCreate(data) {
         staff_group_id: data.staff_group_id[i],
       });
     }
-
     break;
   }
 
   renderCustomer(dataArray);
+  let croped;
+  for (let key in data) {
+    if (key.includes("_")) {
+      let splited = key.replace("_", " ");
+      if (splited.includes("_")) {
+        splited = splited.replace("_", " ");
+      }
+      croped = splited.split(" ");
+      nameArray.push(croped);
+    } else {
+      nameArray.push(key);
+    }
+  }
+  let changed;
+  for (let i = 0; i < nameArray.length; i++) {
+    for (let j = 0; j < nameArray[i].length; j++) {
+      if (typeof nameArray[i] === "string") {
+        changed = nameArray[i].replace(
+          nameArray[i].charAt(0),
+          nameArray[i].charAt(0).toUpperCase()
+        );
+        nameArray[i] = changed;
+        if (nameArray[i].length < 3) {
+          changed = nameArray[i].toUpperCase();
+          nameArray[i] = changed;
+        }
+      }
+      changed = nameArray[i][j].replace(
+        nameArray[i][j].charAt(0),
+        nameArray[i][j].charAt(0).toUpperCase()
+      );
+      nameArray[i][j] = changed;
+      if (nameArray[i][j].length < 3) {
+        changed = nameArray[i][j].toUpperCase();
+        nameArray[i][j] = changed;
+      }
+    }
+
+    let h5 = document.getElementsByClassName("summeryHeader")[i];
+    h5.innerHTML =
+      typeof nameArray[i] === "string"
+        ? nameArray[i] + " Summery"
+        : nameArray[i].join(" ") + " Summery";
+  }
+  calcVisibleData();
 }
 
 function renderCustomer(array) {
-  const dataTable = $("#myTable").DataTable({
+  dataTable = $("#myTable").DataTable({
     autoWidth: false,
     regex: true,
     data: array,
@@ -110,6 +116,11 @@ function renderCustomer(array) {
         data: "valuta_course",
         title: "Valuta Course",
       },
+      {
+        data: "payment_date",
+        title: "Payment Date",
+      },
+
       {
         data: "payment_course",
         title: "Payment Course",
@@ -170,14 +181,6 @@ function renderCustomer(array) {
         "</div>"
     );
     tHeadID++;
-
-    dataTable.columns().every(function () {
-      const column = this;
-
-      $(".indSearch").on("keyup change", function () {
-        column.search(this.value).draw();
-      });
-    });
   });
   table = dataTable;
   selectSearch(table);
@@ -207,7 +210,7 @@ function selectSearch() {
                         <input type="checkbox" id= ${id++ - 1} value= ${
         sortedData[j]
       } />
-                         ${sortedData[j]}
+                     <p className="checkboxData">    ${sortedData[j]}</p>
                         </label>`;
     }
     checkboxes[i].innerHTML = checkedInputs;
@@ -219,23 +222,29 @@ function selectSearch() {
       columnId =
         e.target.parentNode.parentNode.parentNode.previousElementSibling
           .children[1].id;
-
       regex = "";
       if (this.checked) {
         search.push($(this).val());
         for (let i = 0; i < search.length; i++) {
-          i >= 1 ? (regex += "|" + search[i]) : (regex += search[i]);
+          if (i >= 1) {
+            regex += "|" + "^" + search[i];
+          } else {
+            regex += search[i];
+          }
+          // i >= 1 ? (regex += "|" + search[i]) : (regex += search[i]);
         }
         $(e.target.form[0][0]).text($(this).val()).val();
-        table.column(columnId).every(function () {
+        table.column(columnId).each(function () {
           const column = this;
-          column.search(regex, true, false).draw();
+          column.search(regex, true, true).draw();
         });
+        calcVisibleData();
       } else if (!this.checked) {
         table.column(columnId).every(function () {
           const column = this;
           column.search("").draw();
         });
+        calcVisibleData();
       } else {
         let index = search.indexOf($(this).val());
         $(e.target.form[0][0]).text(" ").val();
@@ -244,9 +253,71 @@ function selectSearch() {
           const column = this;
           column.search("").draw();
         });
+        calcVisibleData();
       }
     });
   });
+  // widthRelative();
+}
+
+// function widthRelative() {
+//   $(".multiselect").on("click", function (e) {
+//     let container = e.target.offsetParent.nextElementSibling;
+//     let child = e.target.offsetParent.nextElementSibling.children[1];
+//     let tempVar;
+//     for (let i = 0; i < child.children.length; i++) {
+//       console.log(child.children[1].children[i]);
+//     }
+//   });
+// }
+function calcVisibleData() {
+  let tableRow = document.getElementsByTagName("tr");
+  let container = document.getElementsByClassName("visibleData");
+
+  for (let i = 2; i < tableRow.length; i++) {
+    let currentData;
+    for (let j = 0; j < container.length; j++) {
+      if (tableRow[i].children[j].innerHTML === "No matching records found") {
+        container[j].innerHTML = "Current Data: No Data";
+        console.log(tableRow[i].children[j].innerHTML);
+      } else if (typeof tableRow[i].children[j].innerHTML === "string") {
+        let checked;
+        if (tableRow[i].children[j].innerHTML.includes("-")) {
+          checked = null;
+        } else {
+          checked = parseInt(tableRow[i].children[j].innerHTML);
+        }
+        if (!isNaN(checked)) {
+          if (
+            typeof container[j].innerHTML === "undefined" ||
+            isNaN(parseInt(container[j].innerHTML))
+          ) {
+            container[j].innerHTML = "0";
+            currentData = parseInt(container[j].innerHTML);
+            currentData += checked;
+            container[j].innerHTML = currentData;
+          } else {
+            currentData = parseInt(container[j].innerHTML);
+            currentData += checked;
+            container[j].innerHTML = currentData;
+          }
+        }
+      }
+    }
+  }
+  for (let i = 0; i < container.length; i++) {
+    console.log(container[i].innerHTML);
+
+    if (container[i].innerHTML === "") {
+      container[i].innerHTML = `Current Data: No Data`;
+    } else if (container[i].innerHTML === "Current Data: No Data") {
+      container[i].innerHTML === "Current Data: No Data";
+    } else if (container[i].innerHTML === "Current Data: 0") {
+      container[i].innerHTML === "Current Data: No Data";
+    } else {
+      container[i].innerHTML = `Current Data: ${container[i].innerHTML}`;
+    }
+  }
 }
 function individualSearch() {
   $(".indSearch").on("input", function (e) {
@@ -280,6 +351,107 @@ function searchData(id, container, value) {
     }
   });
   container.innerHTML = checkedInputs;
+}
+
+function styleHtml() {
+  //create footer and header
+  const header = document.createElement("header");
+  const footer = document.createElement("footer");
+
+  const dataTableWrapper = document.getElementById("myTable_wrapper");
+  const dataTableLength = document.getElementById("myTable_length");
+  const dataTableFilter = document.getElementById("myTable_filter");
+  const dataTablePaginate = document.getElementById("myTable_paginate");
+  const dataTableInfo = document.getElementById("myTable_info");
+
+  //create summery div
+  const tHead = document.getElementsByTagName("thead");
+  let tr = document.createElement("tr");
+  tr.classList.add("totalBox");
+  tHead[0].append(tr);
+  // const thLength = document.getElementsByTagName("th");
+
+  //summery row
+  let data = table.columns().data();
+  for (let i = 0; i < 13; i++) {
+    let th = document.createElement("th");
+    th.classList.add("grandTotal");
+    tr.appendChild(th);
+    let div = document.createElement("div");
+    div.classList.add("summerybox");
+    th.appendChild(div);
+    let h5 = document.createElement("h5");
+    div.appendChild(h5);
+    h5.classList.add("summeryHeader");
+    let summeryData = document.createElement("p");
+    let visibleData = document.createElement("p");
+    div.appendChild(summeryData);
+    div.appendChild(visibleData);
+    summeryData.classList.add("summeryData");
+    visibleData.classList.add("visibleData");
+  }
+  totalData();
+
+  let td = document.getElementsByTagName("td");
+  for (let i = 0; i < td.length; i++) {
+    if (td[i].innerHTML.length > 15) {
+      let storedData = td[i].innerHTML;
+      td[i].textContent.slice(15, td[i].textContent.length);
+      td[i].textContent += "...";
+    }
+  }
+
+  function totalData() {
+    let summery = document.getElementsByClassName("summeryData");
+    let visibleData = document.getElementsByClassName("visibleData");
+    for (let i = 0; i < data.length; i++) {
+      let res = 0;
+      let columnData = table.column(i).data();
+      for (let j = 0; j < columnData.length; j++) {
+        if (
+          typeof columnData[j] === "string" &&
+          !columnData[j].includes("-") &&
+          !columnData[j].includes(".")
+        ) {
+          if (!isNaN(parseInt(columnData[j], 10))) {
+            res += parseInt(columnData[j], 10);
+          }
+        }
+      }
+      summery[i].innerHTML = "Total Data: " + res;
+      if (summery[i].innerHTML === "Total Data: 0") {
+        summery[i].innerHTML = "Total Data: No Data";
+      }
+    }
+  }
+
+  //append elements to footer and header
+  header.append(dataTableLength);
+  header.append(dataTableFilter);
+  footer.classList.add("dataTables_wrapper");
+  footer.append(dataTableInfo);
+  footer.append(dataTablePaginate);
+  dataTableWrapper.prepend(header);
+  document.body.append(footer);
+  fixedHeader();
+}
+
+function fixedHeader() {
+  //take tables offset and tableHead
+  let table = document.getElementById("myTable");
+  let tHead = document.getElementsByTagName("thead");
+  let tableWrapper = document.getElementById("myTable_wrapper");
+
+  //on scroll move header with content like a position fixed
+  $(tableWrapper).bind("scroll", function () {
+    let offset = $(this).scrollTop();
+    if (offset >= table.offsetTop) {
+      let position = offset - 12;
+      tHead[0].style.cssText = "transform: translateY(" + position + "px)";
+    } else if (offset < table.offsetTop) {
+      tHead[0].style.cssText = "transform: translateY(-12px)";
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => fetchData(URL));
